76bc66b69567cc8ee2f53e938435730d80728ea3
TonyUUU <tonycl.wu@mail.utoronto.ca>
2022-02-15 16:05:44 -0500

    created cart-item component, connected our cart dropdown with cartItems reducer and implemented quantity count in cart-icon component

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -5,15 +5,24 @@ import './cart-icon.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart-actions';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, cartItems }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>
+      {cartItems.reduce(
+        (quantity, cartItem) => quantity + cartItem.quantity,
+        0
+      )}
+    </span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems: cartItems,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
