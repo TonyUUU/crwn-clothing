664983db82e01ecbdfb368a20dddd5dcd947ab71
TonyUUU <tonycl.wu@mail.utoronto.ca>
2022-02-15 16:10:48 -0500

    move itemCount to mapStateToProps

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -5,20 +5,18 @@ import './cart-icon.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart-actions';
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>
-      {cartItems.reduce(
-        (quantity, cartItem) => quantity + cartItem.quantity,
-        0
-      )}
-    </span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems: cartItems,
+  itemCount: cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  ),
 });
 
 const mapDispatchToProps = (dispatch) => ({
